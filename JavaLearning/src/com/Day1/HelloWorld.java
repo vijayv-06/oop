e462b1b15e4bd2a7d2072ccaf3ac62c4c335f3a2
Vijay v <vijayvenkatesh841@gmail.com>
2025-10-28 21:11:18 +0530

    dsdasd

---
+++ JavaLearning/src/com/Day1/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.Basics;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.println("This is the line from github");
+    }
+}
